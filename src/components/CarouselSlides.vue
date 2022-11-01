502f4ccd6a650995381457e3bf54a7808cfbded5
haythemnouigui <ha.nouigui@gmail.com>
2022-11-01 13:00:57 +0100

    carousel

---
+++ src/components/CarouselSlides.vue
@@ -0,0 +1,280 @@
+<template>
+  <div class="container">
+    <div id="carousel1">
+      <div class="item">
+        <!-- <a href="https://vuejs.org" target="_blank" rel="noopener"> -->
+        <div class="item__img">
+          <img
+            src="images/christophe-rollando-EeYOkwd3z3c-unsplash.jpeg"
+            alt=""
+          />
+        </div>
+        <div class="item__body">
+          <div class="item__title">Lorem ipsum dolor sit 1</div>
+          <div class="item__description">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis
+            hic aperiam possimus voluptates minus sit earum sapiente quidem
+            laborum repellat!
+          </div>
+        </div>
+        <!-- </a> -->
+      </div>
+
+      <div class="item">
+        <div class="item__img">
+          <img
+            src="images/christophe-rollando-f6QRK_K6YyI-unsplash.jpeg"
+            alt=""
+          />
+        </div>
+        <div class="item__body">
+          <div class="item__title">Lorem ipsum dolor sit 2</div>
+          <div class="item__description">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis
+            hic aperiam possimus voluptates minus sit earum sapiente quidem
+            laborum repellat!
+          </div>
+        </div>
+      </div>
+
+      <div class="item">
+        <div class="item__img">
+          <img src="images/masahiro-miyagi-6xOKRgbNDog-unsplash.jpeg" alt="" />
+        </div>
+        <div class="item__body">
+          <div class="item__title">Lorem ipsum dolor sit 3</div>
+          <div class="item__description">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis
+            hic aperiam possimus voluptates minus sit earum sapiente quidem
+            laborum repellat!
+          </div>
+        </div>
+      </div>
+
+      <div class="item">
+        <div class="item__img">
+          <img src="images/raphael-andres-a7LbGuyOkQA-unsplash.jpeg" alt="" />
+        </div>
+        <div class="item__body">
+          <div class="item__title">Lorem ipsum dolor sit 4</div>
+          <div class="item__description">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis
+            hic aperiam possimus voluptates minus sit earum sapiente quidem
+            laborum repellat!
+          </div>
+        </div>
+      </div>
+
+      <div class="item">
+        <div class="item__img">
+          <img src="images/sean-lee-8p02cxiNGl4-unsplash.jpeg" alt="" />
+        </div>
+        <div class="item__body">
+          <div class="item__title">Lorem ipsum dolor sit 5</div>
+          <div class="item__description">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis
+            hic aperiam possimus voluptates minus sit earum sapiente quidem
+            laborum repellat!
+          </div>
+        </div>
+      </div>
+
+      <div class="item">
+        <div class="item__img">
+          <img src="images/masahiro-miyagi-6xOKRgbNDog-unsplash.jpeg" alt="" />
+        </div>
+        <div class="item__body">
+          <div class="item__title">Lorem ipsum dolor sit 6</div>
+          <div class="item__description">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis
+            hic aperiam possimus voluptates minus sit earum sapiente quidem
+            laborum repellat!
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div id="carousel2">
+      <div class="item">
+        <div class="item__img">
+          <img
+            src="images/christophe-rollando-f6QRK_K6YyI-unsplash.jpeg"
+            alt=""
+          />
+        </div>
+        <div class="item__body">
+          <div class="item__title">Lorem ipsum dolor sit 2</div>
+          <div class="item__description">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis
+            hic aperiam possimus voluptates minus sit earum sapiente quidem
+            laborum repellat!
+          </div>
+        </div>
+      </div>
+
+      <div class="item">
+        <div class="item__img">
+          <img src="images/masahiro-miyagi-6xOKRgbNDog-unsplash.jpeg" alt="" />
+        </div>
+        <div class="item__body">
+          <div class="item__title">Lorem ipsum dolor sit 3</div>
+          <div class="item__description">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis
+            hic aperiam possimus voluptates minus sit earum sapiente quidem
+            laborum repellat!
+          </div>
+        </div>
+      </div>
+
+      <div class="item">
+        <div class="item__img">
+          <img src="images/raphael-andres-a7LbGuyOkQA-unsplash.jpeg" alt="" />
+        </div>
+        <div class="item__body">
+          <div class="item__title">Lorem ipsum dolor sit 4</div>
+          <div class="item__description">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis
+            hic aperiam possimus voluptates minus sit earum sapiente quidem
+            laborum repellat!
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div id="carousel3">
+      <div class="item">
+        <div class="item__img">
+          <img
+            src="images/christophe-rollando-f6QRK_K6YyI-unsplash.jpeg"
+            alt=""
+          />
+        </div>
+        <div class="item__body">
+          <div class="item__title">Lorem ipsum dolor sit 2</div>
+          <div class="item__description">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis
+            hic aperiam possimus voluptates minus sit earum sapiente quidem
+            laborum repellat!
+          </div>
+        </div>
+      </div>
+
+      <div class="item">
+        <div class="item__img">
+          <img src="images/masahiro-miyagi-6xOKRgbNDog-unsplash.jpeg" alt="" />
+        </div>
+        <div class="item__body">
+          <div class="item__title">Lorem ipsum dolor sit 3</div>
+          <div class="item__description">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis
+            hic aperiam possimus voluptates minus sit earum sapiente quidem
+            laborum repellat!
+          </div>
+        </div>
+      </div>
+
+      <div class="item">
+        <div class="item__img">
+          <img src="images/raphael-andres-a7LbGuyOkQA-unsplash.jpeg" alt="" />
+        </div>
+        <div class="item__body">
+          <div class="item__title">Lorem ipsum dolor sit 4</div>
+          <div class="item__description">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis
+            hic aperiam possimus voluptates minus sit earum sapiente quidem
+            laborum repellat!
+          </div>
+        </div>
+      </div>
+
+      <div class="item">
+        <div class="item__img">
+          <img
+            src="images/christophe-rollando-f6QRK_K6YyI-unsplash.jpeg"
+            alt=""
+          />
+        </div>
+        <div class="item__body">
+          <div class="item__title">Lorem ipsum dolor sit 2</div>
+          <div class="item__description">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis
+            hic aperiam possimus voluptates minus sit earum sapiente quidem
+            laborum repellat!
+          </div>
+        </div>
+      </div>
+
+      <div class="item">
+        <div class="item__img">
+          <img src="images/masahiro-miyagi-6xOKRgbNDog-unsplash.jpeg" alt="" />
+        </div>
+        <div class="item__body">
+          <div class="item__title">Lorem ipsum dolor sit 3</div>
+          <div class="item__description">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis
+            hic aperiam possimus voluptates minus sit earum sapiente quidem
+            laborum repellat!
+          </div>
+        </div>
+      </div>
+
+      <div class="item">
+        <div class="item__img">
+          <img src="images/raphael-andres-a7LbGuyOkQA-unsplash.jpeg" alt="" />
+        </div>
+        <div class="item__body">
+          <div class="item__title">Lorem ipsum dolor sit 4</div>
+          <div class="item__description">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis
+            hic aperiam possimus voluptates minus sit earum sapiente quidem
+            laborum repellat!
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+
+
+import Carousel from "../test.js";
+
+export default {
+  data() {
+    return {
+
+    };
+  },
+  components: {},
+  methods: {
+    caro() {
+      new Carousel(document.querySelector("#carousel1"), {
+        slidesToScroll: 1,
+        slidesVisible: 3,
+        // loop:true,
+        pagination: true,
+        // infinite: true,
+      });
+    },
+    caro1() {
+      new Carousel(document.querySelector("#carousel2"), {});
+    },
+    caro2() {
+      new Carousel(document.querySelector("#carousel3"), {
+        slidesToScroll: 2,
+        slidesVisible: 2,
+        infinite: true,
+        pagination: true,
+      });
+    },
+  },
+  beforeMount() {},
+  mounted() {
+    this.caro();
+    this.caro1();
+    this.caro2();
+
+
+  },
+};
+</script>
